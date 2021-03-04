535971ab747c7200f26bb4c52190b181e2a47780
RossDeVito <rdevito97@gmail.com>
2021-03-04 16:35:29 -0500

    updated ground truth

--- walkthrough.py
+++ walkthrough.py
@@ -6,30 +6,27 @@ from utils import *
 
 if __name__ == '__main__':
 	fragments_path='data/fragments/chr20_1-500K/fragments.txt'
-	longshot_vcf_path='data/fragments/chr20_1-500K/1.0.potential_SNVs.vcf'
+	longshot_vcf_path='data/fragments/chr20_1-500K/2.0.realigned_genotypes.vcf'
 	ground_truth_vcf_path='data/GIAB/HG002_GRCh38_1_22_v4.1_draft_benchmark.vcf'
+	giab_bed_path='data/GIAB/HG002_GRCh38_1_22_v4.1_draft_benchmark.bed'
 	
-	# load read fragments and the quality of each read location
+	# load read fragments and their qualities
 	_, fragments, qualities = read_fragments(fragments_path)
 
-	print('Fragments:')
+	print('All fragments:')
 	matrix_sparsity_info(fragments, print_info=True)
 
 	# get real/false variant labels
-	variant_labels = get_true_variants(
+	variant_labels, site_mask, ls_callset, callset = get_true_variants(
 		longshot_vcf_path=longshot_vcf_path,
-		ground_truth_vcf_path=ground_truth_vcf_path
+		ground_truth_vcf_path=ground_truth_vcf_path,
+		giab_bed_path=giab_bed_path,
+		return_vcfs=True
 	)
 
-	# given fragments (and possibly qualities) label each col as a real (1)
-	#	or false (0) variant. Simple baselines are shown below:
+	# mask these with site_mask
+	fragments = fragments[:, site_mask]
+	qualities = qualities[:, site_mask]
 
-	# predict all varients are false
-	pred_labels = np.zeros_like(variant_labels)
-	cr = classification_report(variant_labels, pred_labels, zero_division=0)
-	print('\nPredict all false:\n' + cr)
-
-	# predict random
-	rand_labels = np.random.choice((0,1), size=variant_labels.shape)
-	cr = classification_report(variant_labels, rand_labels)
-	print('\nPredict randomly:\n' + cr)
+	print('\nMasked fragments:')
+	matrix_sparsity_info(fragments, print_info=True)
